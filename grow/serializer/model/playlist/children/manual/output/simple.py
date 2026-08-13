9440ea42669b9529e1ff58e7a727a62a74347449
mignot <garcia.andreas.1991@gmail.com>
2026-08-13 09:41:29 +0200

    feat(playlist,manual-playlist): add reference/playlists and reference/manual-playlists viewsets, serializers, filtersets

---
+++ grow/serializer/model/playlist/children/manual/output/simple.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from the_music_tree_api_kit.serializer.field.AppCharField import AppCharField
+
+from grow.model.playlist.children.manual.ManualPlaylist import ManualPlaylist
+
+from .Fields import Fields
+
+
+class ManualPlaylistSimpleSerializer(serializers.ModelSerializer):
+    name = AppCharField()
+    uploaded_tracks_count = serializers.IntegerField(source=Fields.UPLOADED_TRACKS_NOT_ARCHIVED_COUNT_INTERNAL)
+
+    class Meta:
+        model = ManualPlaylist
+        fields = [
+            Fields.UUID,
+            Fields.NAME_PUBLIC,
+            Fields.UPLOADED_TRACKS_NOT_ARCHIVED_COUNT_PUBLIC,
+            Fields.CREATED_ON,
+        ]
