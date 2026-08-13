8ba13a7484a4c0e1bbfc118788001cf70d75e244
mignot <garcia.andreas.1991@gmail.com>
2026-08-13 09:47:33 +0200

    feat(urls): register reference/playlists, reference/manual-playlists, reference/library/uploaded routes

--- grow/urls.py
+++ grow/urls.py
@@ -6,6 +6,9 @@ from grow.view.viewset.model.album.AlbumViewSet import AlbumViewSet
 from grow.view.viewset.model.artist.ArtistViewSet import ArtistViewSet
 from grow.view.viewset.model.criteria.children.genre.GenreViewSet import GenreViewSet
 from grow.view.viewset.model.criteria.children.tag.TagViewSet import TagViewSet
+from grow.view.viewset.model.playlist.children.manual.ManualPlaylistViewSet import ManualPlaylistViewSet
+from grow.view.viewset.model.playlist.PlaylistViewSet import PlaylistViewSet
+from grow.view.viewset.model.uploaded_track.UploadedTrackViewSet import UploadedTrackViewSet
 
 router = routers.DefaultRouter()
 
@@ -15,6 +18,9 @@ router.register(r"reference/artists", ArtistViewSet, basename="reference-artist"
 router.register(r"reference/albums", AlbumViewSet, basename="reference-album")
 router.register(r"reference/genres", GenreViewSet, basename="reference-genre")
 router.register(r"reference/tags", TagViewSet, basename="reference-tag")
+router.register(r"reference/playlists", PlaylistViewSet, basename="reference-playlist")
+router.register(r"reference/manual-playlists", ManualPlaylistViewSet, basename="reference-manual-playlist")
+router.register(r"reference/library/uploaded", UploadedTrackViewSet, basename="reference-uploaded-track")
 
 urlpatterns = [
     path("health/", HealthCheckView.as_view(), name="health"),
